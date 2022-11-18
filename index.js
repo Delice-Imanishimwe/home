554d1c043e14762d97c160dfc9fd09465657ff44
Delice-Imanishimwe <deliceimanashimwe@gmail.com>
2022-11-18 01:59:01 -0800

    this is the first application

---
+++ index.js
@@ -0,0 +1,15 @@
+function check(names,age){
+    console.log("hello",names,"yuor age is:",age,"and");
+    if(age<20){
+        console.log("you are stear young")
+    }
+    else{
+        console.log("your are an adult")
+    }
+}
+check("delice",19)
+function check(names,age){
+    let takeAges=(age>20) ? "your are stear young":"your are an adult"
+    console.log("takeAge")
+}
+check("delice",25)
